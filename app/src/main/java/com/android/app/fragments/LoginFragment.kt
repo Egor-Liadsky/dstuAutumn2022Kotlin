747b4ad77e05bb3bdb24c833b04b8f0af257330d
Egor Liadsky <47715067+Egor-Liadsky@users.noreply.github.com>
2022-10-22 02:57:50 +0300

    доработка интерфейса

--- app/src/main/java/com/android/app/fragments/LoginFragment.kt
+++ app/src/main/java/com/android/app/fragments/LoginFragment.kt
@@ -26,9 +26,9 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
 
-        binding.registrationButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
-        }
+//        binding.registrationButton.setOnClickListener {
+//            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+//        }
 
         return binding.root
     }

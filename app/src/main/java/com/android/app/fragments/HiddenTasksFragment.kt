33e24e68f9504c29d06df3c7b1897a9f4245cd2d
Egor Liadsky <47715067+Egor-Liadsky@users.noreply.github.com>
2022-10-22 03:49:16 +0300

    доработка интерфейса

--- app/src/main/java/com/android/app/fragments/HiddenTasksFragment.kt
+++ app/src/main/java/com/android/app/fragments/HiddenTasksFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.android.app.R
 
 
@@ -15,7 +17,16 @@ class HiddenTasksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setupToolbar()
+
         return inflater.inflate(R.layout.fragment_hidden_tasks, container, false)
+    }
+
+    fun setupToolbar(){
+        val toolbarMain = view?.findViewById<Toolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).apply {
+            setSupportActionBar(toolbarMain)
+            toolbarMain?.inflateMenu(R.menu.toolbar_menu)
+        }
     }
 }
--- app/src/main/java/com/android/app/fragments/LoginFragment.kt
+++ app/src/main/java/com/android/app/fragments/LoginFragment.kt
@@ -1,13 +1,12 @@
 package com.android.app.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.android.app.R
 import com.android.app.databinding.FragmentLoginBinding
@@ -22,14 +21,25 @@ class LoginFragment : Fragment() {
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+
         if (1 > 2){
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
+
+
 
         binding.nextTextView.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
         }
 
         return binding.root
+    }
+
+    fun setupToolbar(){
+        val toolbarMain = view?.findViewById<Toolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).apply {
+            setSupportActionBar(toolbarMain)
+            toolbarMain?.inflateMenu(R.menu.toolbar_menu)
+        }
     }
 }

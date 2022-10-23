8cc3c95a94e8b28bbdb7b7c8f3e4e31a4d5c7965
Vanya-761 <116350928+Vanya-761@users.noreply.github.com>
2022-10-23 10:05:46 +0300

    Работа со сменой темы

--- app/src/main/java/com/android/app/fragments/SettingsFragment.kt
+++ app/src/main/java/com/android/app/fragments/SettingsFragment.kt
@@ -27,7 +27,7 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
-
+        binding.changeThemeButton.text = Constants.THEME
         binding.changeThemeButton.setOnClickListener {
 //            if (Constants.THEME == "light"){
 //                binding.changeThemeButton.text = "ТЕМНАЯ"
@@ -37,14 +37,14 @@ class SettingsFragment : Fragment() {
 //            }
             when(resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
                 Configuration.UI_MODE_NIGHT_YES -> {
-                    Constants.THEME = "ТЕМНАЯ"
-                    binding.changeThemeButton.text = Constants.THEME
+                    Constants.THEME = "ЗЕЛЕНАЯ"
+//                    binding.changeThemeButton.text = Constants.THEME
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                     viewModel.preferencesStore.saveTheme(true)
                 }
                 Configuration.UI_MODE_NIGHT_NO -> {
-                    Constants.THEME = "СВЕТЛАЯ"
-                    binding.changeThemeButton.text = Constants.THEME
+                    Constants.THEME = "СИНЯЯ"
+//                    binding.changeThemeButton.text = Constants.THEME
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                     viewModel.preferencesStore.saveTheme(false)
                 }

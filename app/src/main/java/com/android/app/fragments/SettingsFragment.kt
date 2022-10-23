3c6e600c15c873a08bee9af8bb7081fee4e42622
Vanya-761 <116350928+Vanya-761@users.noreply.github.com>
2022-10-23 09:48:27 +0300

    Доработка интерфейса

--- app/src/main/java/com/android/app/fragments/SettingsFragment.kt
+++ app/src/main/java/com/android/app/fragments/SettingsFragment.kt
@@ -25,7 +25,7 @@ class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
         binding.changeThemeButton.setOnClickListener {
